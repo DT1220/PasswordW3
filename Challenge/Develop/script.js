f0889ab7b36d43238ecc89126286593119336558
Daniel <danieltang@Daniels-MacBook-Pro.local>
2022-06-16 17:09:16 -0500

    second commit

--- Challenge/Develop/script.js
+++ Challenge/Develop/script.js
@@ -9,6 +9,25 @@ var uppercaseCheck;
 var numberCheck;
 var specialCheck;
 
+//Function used to determine the length of the password
+function determineLength(){
+  passwordLength = prompt("How many characters would you like your password to be? (8-128 characters): ");
+
+    if (passwordLength<8){
+      alert("Password must be a number between 8-128 characters");
+      determineLength();
+    }else if (passwordLength>128){
+      alert("Password must be a number between 8-128 characters");
+      determineLength();
+    }else if (isNaN(passwordLength)){
+      alert("Password must be a number between 8-128 characters");
+      determineLength();
+    }else{
+    alert("The next three screens will ask you what types of characters you would like to be included in your password.\nIf you choose 'No' for all, your password will only contain lowercase letters.");
+    }
+    return passwordLength;
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
